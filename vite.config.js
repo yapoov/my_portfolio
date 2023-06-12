e6657e711cb2f83355eed64a329e0afa94688caf
orgil <orgilo148@gmail.com>
2023-06-12 19:53:43 +0800

    dd

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	base: '/my_portfolio/',
+	plugins: [sveltekit()],
+	build: {
+		outDir: './dist' // relative to index.html
+		// emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+	}
 });
