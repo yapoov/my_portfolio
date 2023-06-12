e74c4c5b316fc0bba74e691e92fec37d83d1b8d5
orgil <orgilo148@gmail.com>
2023-06-12 20:10:39 +0800

    dd

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
-const dev = process.argv.includes('dev');
+
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH
+			base: dev ? '' : '/my_portfolio'
 		}
 	}
 };
+
 export default config;
